ea2ab6ad5f6a3f15aa61807077704ec2756691b1
yashsnehalshah <yash.shah2993@gmail.com>
2016-12-13 01:03:03 -0500

    More project changes

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //configure a public directory to host static content
 app.use(express.static(__dirname + '/public'));
 
-//require ("./test/app.js")(app);
+
 //require("./assignment/app.js")(app);
 require("./project/app.js")(app);
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
